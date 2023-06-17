8e81f9f574ac7731eac5c98591b82bc406bd2918
riadh-tabesto <riadhmezzi@tabesto.com>
2023-06-17 18:41:07 +0200

    FEAT: show teams list

---
+++ app/src/main/java/com/riadh/leagues/main/ImageAdapter.kt
@@ -0,0 +1,32 @@
+package com.riadh.leagues.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.riadh.leagues.R
+
+class ImageAdapter(private val imageList: List<String>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.badge_item_layout, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageUrl = imageList[position]
+        holder.imageView.load(data = imageUrl) {
+            placeholder(R.drawable.soccer)
+        }
+    }
+
+    override fun getItemCount() = imageList.size
+}
--- app/src/main/java/com/riadh/leagues/main/MainActivity.kt
+++ app/src/main/java/com/riadh/leagues/main/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.riadh.leagues.main
 
 import android.os.Bundle
-import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.riadh.leagues.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +16,9 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
-    lateinit var adapter: ArrayAdapter<String>
+    private lateinit var leaguesAdapter: ArrayAdapter<String>
+    private lateinit var imageAdapter: ImageAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,13 +33,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.teamsList.layoutManager = GridLayoutManager(this, 2)
     }
 
     private fun manageSearch() {
         viewModel.leaguesLiveDataList.observe(this) { leaguesList ->
-            adapter =
+            leaguesAdapter =
                 ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, leaguesList)
-            binding.searchBar.setAdapter(adapter)
+            binding.searchBar.setAdapter(leaguesAdapter)
         }
     }
 
@@ -48,7 +52,7 @@ class MainActivity : AppCompatActivity() {
     private fun manageClick() {
         binding.searchBar.onItemClickListener =
             AdapterView.OnItemClickListener { _, _, position, _ ->
-                val selectedItem = adapter.getItem(position)
+                val selectedItem = leaguesAdapter.getItem(position)
                 selectedItem?.let {
                     viewModel.fetchLeagueTeams(it)
                 }
@@ -57,7 +61,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun showTeamsList() {
         viewModel.teamsLiveDataList.observe(this) { teamsList ->
-            Log.i("list", teamsList.size.toString())
+            hideKeyboard()
+            imageAdapter = ImageAdapter(teamsList)
+            binding.teamsList.adapter = imageAdapter
+        }
+    }
+
+    //move to util class
+    private fun hideKeyboard() {
+        this.currentFocus?.let { view ->
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
 
